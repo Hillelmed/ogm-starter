7342b8bb436cc19aa7876c26dd4e113bd67eb540
hillel <hillel95med@gmail.com>
2022-01-03 21:13:51 +0200

    ad

---
+++ src/test/java/com/hillel/ogm/Ogm/OgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hillel.ogm.Ogm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
